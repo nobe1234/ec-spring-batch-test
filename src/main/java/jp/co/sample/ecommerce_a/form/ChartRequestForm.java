1f14e6411b9ffdfcea1838e62996664a88343f23
sohei.nobe <sohei.nobe@rakus.co.jp>
2018-12-17 16:19:50 +0900

    プルダウン選択されたリクエストを受け取るフォームを作成

---
+++ src/main/java/jp/co/sample/ecommerce_a/form/ChartRequestForm.java
@@ -0,0 +1,21 @@
+package jp.co.sample.ecommerce_a.form;
+
+/**
+ * プルダウンで選択された内容のリクエストを受け取るエンティティ.
+ * 
+ * @author soheinobe
+ *
+ */
+public class ChartRequestForm {
+
+	private Integer year;
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+}
