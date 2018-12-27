443d719aafd699e2ee35ffbc02c8b96f322f1e24
sohei.nobe <sohei.nobe@rakus.co.jp>
2018-12-27 13:40:37 +0900

    jsのコメントを削除

--- src/main/webapp/js/clickedPaging.js
+++ src/main/webapp/js/clickedPaging.js
@@ -1,19 +1,5 @@
 $(function() {
-	// var pageNumber = $('#pageNumber').val();
-	// var pageNumber = $(#);
-	// alert("ロードした時のページ番号：" + pageNumber); // なぜか１０が送られない
 
-	// page読み込み時に今いるページへのリンクボタンを非活性化したい。
-	// $('#pageNumber').val() = 10;
-	// alert("代入できているチェック：" + $("[name='pageNumber']").val()); // なぜか１０が送られない
-
-	// まず最初のロード時は１番目のページ番号をおふにしておく。
-	// $('#pageOneLink').prop("disabled", true).css({
-	// 'background-color' : 'gray',
-	// 'color' : 'white'
-	// });
-
-	alert(getParam('pageNumber'));
 	var pageNumber = getParam('pageNumber');
 	/**
 	 * Get the URL parameter value
@@ -51,55 +37,9 @@ $(function() {
 
 	$('#pageOneLink').on('click', function() {
 		$('#page1Form').submit();
-		// pageNumber = $("#pageNumber1").val();
-		// alert(pageNumber);
-		// onLoad();
 	});
 
 	$('#pageTwoLink').on('click', function() {
 		$('#page2Form').submit();
-		// pageNumber = $("#pageNumber2").val();
-		// // 0が入る
-		// alert("クリックした時のページ番号：" + pageNumber);
-		// onLoad();
 	});
 });
-
-// $(function() {
-// $('#pageOneLink').submit(function() {
-// $('#page1Form').submit();
-// $('#pageOneLink').prop('disabled', true);
-// $('#pageTwoLink').prop('disabled', false);
-// });
-//
-// $('#pageTwoLink').on('click', function() {
-// var clickValue = $('#pageTwoLink').val();
-// alert(clickValue);
-// $('#page2Form').submit();
-// $('#pageTwoLink').prop('disabled', true);
-// $('#pageOneLink').prop('disabled', false);
-// // document.pageForm.submit();
-//
-// });
-//
-// });
-// うまく動かない
-
-//
-// let pageOne = document.getElementById('#pageOneLink');
-// let pagetwo = document.getElementById('#pageTwoLink');
-//
-// let pageForm = document.getElementById('#pageForm');
-//
-// //pageFormがあるかをかくにんする。
-// if(page1Form){
-// pageForm.addEventListener('click', (evt) => {
-// pageOne.disabled = true;
-// pagetwo.disabled = false
-// }, false);
-// }
-
-// pageForm.addEventListener('click', (evt) => {
-// pageOne.disabled = false;
-// pagetwo.disabled = true;
-// }, false);
